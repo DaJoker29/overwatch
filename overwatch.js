6b07cde233802548b3b544c1ed0b23c79851b422
Dewitt Buckingham <dj729@comcast.net>
2016-11-21 19:16:29 -0800

    Schedule

--- overwatch.js
+++ overwatch.js
@@ -1,14 +1,57 @@
 const schedule = require('node-schedule');
 const monitor = require('./lib/monitor.js');
 
-const interval = '*/15 * * * *';
+const interval = {
+  service: process.env.SERVICE_INTERVAL || '*/15 * * * *',
+  site: process.env.SITE_INTERVAL || '1 * * * *',
+};
+
+
 /**
  * Overwatch - Enhanced Monitoring
  * @module Overwatch
  */
 
 console.log('launching overwatch...');
-// schedule.scheduleJob(interval, monitor.run);
+
+// Run service checks every 30 minutes
+const services = schedule.scheduleJob(interval.service, () => {
+  monitor.serviceChecks(handleCheck);
+});
+
+// Run website checks once an hour
+const sites = schedule.scheduleJob(interval.site, () => {
+  monitor.siteChecks(handleCheck);
+});
+
+function handleCheck(err, changes) {
+  if (err) {
+    console.error(err);
+  }
+
+  if (changes) {
+    const message = buildMsg(changes);
+    console.log(message);
+  }
+}
+
+function buildMsg(changes) {
+  const msg = [`Overwatch has detected a change on the following monitored property.\n`];
+  if (changes.added) {
+    changes.added.forEach((e) => {
+      msg.push(`Up: ${e.resolved || e.url}\n`);
+    });
+  }
+
+  if (changes.removed) {
+    changes.removed.forEach((e) => {
+      msg.push(`Down: ${e.resolved || e.url}\n`);
+    });
+  }
+
+  msg.push('\nAll changes reported.\n');
+  return msg.join('');
+}
 
 /**
  * Handle uncaught exceptions
